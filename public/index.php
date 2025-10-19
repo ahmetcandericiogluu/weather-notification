839d06cc0f00d9ae317dd8bd986ae514dd3ce60d
ahmetcandericiogluu <4hcan2001@gmail.com>
2025-10-19 23:36:49 +0300

    Fix: ignore missing .env in production

--- public/index.php
+++ public/index.php
@@ -1,9 +1,20 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+// Eğer .env dosyası varsa (local ortamda) yükle, yoksa hatayı yoksay
+$envPath = dirname(__DIR__).'/.env';
+if (file_exists($envPath)) {
+    (new Dotenv())->bootEnv($envPath);
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'prod', (bool) ($_SERVER['APP_DEBUG'] ?? false));
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
